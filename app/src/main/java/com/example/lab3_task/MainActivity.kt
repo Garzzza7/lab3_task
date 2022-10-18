e460fb397f84d916ed6e1c99600d5511cdef4eea
Garzzza7 <piotr.czapela@gmail.com>
2022-10-18 21:45:25 +0200

    Corrections

--- app/src/main/java/com/example/lab3_task/MainActivity.kt
+++ app/src/main/java/com/example/lab3_task/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.lab3_task
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+/*
         val A:EditText=findViewById(R.id.Ainput);
         val B:EditText=findViewById(R.id.Binput);
         val C:EditText=findViewById(R.id.Cinput);
@@ -29,35 +27,47 @@ class MainActivity : AppCompatActivity() {
         btn.setOnClickListener{
             txt.text=dicriminant(AA,BB,CC)
         }
-
-    }
-
-    fun dicriminant(a: Double, b: Double, c: Double): String {
-        val root1: Double;
-        val root2: Double;
-        val result: String;
-        val determinant = a*a;
-        if(determinant>0){
-            root1=(-b + Math.sqrt(determinant))/(2*a);
-            root2=(-b - Math.sqrt(determinant))/(2*a);
-            result = "root1 is %.2f and root2 is %.2f".format(root1,root2);
-            return result;
+*/
+        val A = findViewById<EditText>(R.id.Ainput)
+        val B = findViewById<EditText>(R.id.Binput)
+        val C = findViewById<EditText>(R.id.Cinput)
 
 
+        val button = findViewById<Button>(R.id.calculate_button)
+
+        val result = findViewById<TextView>(R.id.textView2)
+
+        button.setOnClickListener{
+
+            var vala = A.text.toString().toInt()
+            var valb = B.text.toString().toInt()
+            var valc = C.text.toString().toInt()
+            var root1: Double
+            var root2: Double
+            var result1: String;
+
+            var dicriminant = valb*valb-4*vala*valc
+            val realPart = -valb / (2 * vala)
+            val imaginaryPart = Math.sqrt((-dicriminant).toDouble()) / (2 * vala)
+
+            if(dicriminant>0){
+                root1= ((-valb + Math.sqrt(dicriminant.toDouble()))/(2*vala));
+                root2= ((-valb - Math.sqrt(dicriminant.toDouble()))/(2*vala));
+                result1 = "root1 is %.2f and root2 is %.2f".format(root1,root2);
+                result.setText(root1.toString()+" and "+root2.toString())
+                //result.setText(determinant.toString())
+            }
+            else if(dicriminant==0){
+                root1 = ((-valb/(2*vala)).toDouble())
+                result1="There is only one root %.2f".format(root1);
+                result.setText("Only one solution: "+root1.toString())
+                //result.setText(determinant.toString())
+            }
+            else if(dicriminant<0.0){
+                result.setText(realPart.toString()+"+"+imaginaryPart.toString()+"i  and  "+realPart.toString()+"-"+imaginaryPart.toString()+"i")
+            }
         }
-        else if(determinant==0.0){
-            root1 = -b/(2*a);
-            result="There is only one root %.2f".format(root1);
-            return result;
-
-
-        }
-        else {
-            result="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-            return result;
-
-        }
-
-
     }
 }
+
+
